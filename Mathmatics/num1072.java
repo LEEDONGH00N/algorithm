114caf8f93ba66a64360c4b2600e3136c3243826
LeeDongHoon <likelion.leedh@gmail.com>
2024-02-19 14:05:24 +0900

    백준 1072번 (게임)

---
+++ Mathmatics/num1072.java
@@ -0,0 +1,15 @@
+package Mathmatics;
+
+import java.util.Scanner;
+
+public class num1072 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long x = sc.nextInt(), y = sc.nextInt();
+        int z = (int) (y * 100 / x);
+        int ans = -1;
+        if (z < 99)
+            ans = (int) Math.ceil((100 * y - x * (z + 1)) / (double) (z - 99));
+        System.out.println(ans);
+    }
+}
