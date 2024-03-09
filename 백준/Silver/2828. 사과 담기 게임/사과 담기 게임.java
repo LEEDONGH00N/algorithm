88db4c6ab34f02bc072ac5d7992bdf12311cc53a
LEEDONGH00N <123933574+LEEDONGH00N@users.noreply.github.com>
2024-03-09 16:12:30 +0900

    [Silver V] Title: 사과 담기 게임, Time: 120 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.java
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.java
@@ -4,23 +4,23 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n; static int m;static int bucket_location = 1;static int sum = 0;
-    static int[] location;
+    static int n; static int m;static int bucket_start = 1;static int sum = 0; static int bucket_last;
     static void solution(int locate){
-        if(bucket_location < locate)
-        for(int i = 0; bucket_location + (m-1) + i <= n; i++){
-            if((bucket_location + i <= locate)
-                    &&(bucket_location + (m-1) + i >= locate)){
-
-                bucket_location += i; sum += i;
-                return;
+        bucket_last = bucket_start + (m-1);
+        if(bucket_start < locate){
+            for(int i = 0; bucket_last + i <= n; i++){
+                if((bucket_start + i <= locate)
+                        &&(bucket_last + i >= locate)){
+                    bucket_start += i; sum += i;
+                    return;
+                }
             }
         }
         else{
-            for(int i = 0; bucket_location - i >= 1; i++){
-                if((bucket_location - i <= locate)
-                        &&(bucket_location + (m-1) - i  >= locate)){
-                    bucket_location -= i; sum += i;
+            for(int i = 0; bucket_start - i >= 1; i++){
+                if((bucket_start - i <= locate)
+                        &&(bucket_last - i  >= locate)){
+                    bucket_start -= i; sum += i;
                     return;
                 }
             }
@@ -30,16 +30,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken()); m = Integer.parseInt(st.nextToken());
+
         int apple = Integer.parseInt(br.readLine());
-        location = new int[apple];
-        for(int i = 0; i < apple; i++)
-            location[i] = Integer.parseInt(br.readLine());
 
-        for(int locate : location)
-            solution(locate);
-
+        for(int i = 0; i < apple; i++) {
+            int location = Integer.parseInt(br.readLine());
+            solution(location);
+        }
         System.out.println(sum);
     }
 }
