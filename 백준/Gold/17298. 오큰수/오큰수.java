ba83c4a03dee752e823a497df19865a24660ff5a
LEEDONGH00N <123933574+LEEDONGH00N@users.noreply.github.com>
2024-03-29 17:44:28 +0900

    [Gold IV] Title: 오큰수, Time: 1056 ms, Memory: 150900 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Main {
+    static int N;
+    static int[] num;
+    static int[] tmp;
+    static Stack<Integer> stack = new Stack<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        num = new int[N];
+        tmp = new int[N];
+        Arrays.fill(tmp, -1);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            num[i] = Integer.parseInt(st.nextToken());
+            while(!stack.isEmpty() && num[stack.peek()] < num[i]){
+                tmp[stack.peek()] = num[i];
+                stack.pop();
+            }
+            stack.push(i);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i < N;i++){
+            sb.append(tmp[i]);
+            sb.append(" ");
+        }
+        System.out.println(sb);
+    }
+}
