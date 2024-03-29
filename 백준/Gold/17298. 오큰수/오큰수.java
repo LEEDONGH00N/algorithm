eba5054dcae031a1024cde55a5cbe23b79716de4
LEEDONGH00N <123933574+LEEDONGH00N@users.noreply.github.com>
2024-03-29 17:50:30 +0900

    [Gold IV] Title: 오큰수, Time: 1052 ms, Memory: 150756 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -11,23 +11,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        num = new int[N];
-        tmp = new int[N];
+        num = new int[N]; tmp = new int[N];
         Arrays.fill(tmp, -1);
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++){
             num[i] = Integer.parseInt(st.nextToken());
-            while(!stack.isEmpty() && num[stack.peek()] < num[i]){
-                tmp[stack.peek()] = num[i];
-                stack.pop();
-            }
+            while(!stack.isEmpty() && num[stack.peek()] < num[i])
+                tmp[stack.pop()] = num[i];
             stack.push(i);
         }
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i < N;i++){
-            sb.append(tmp[i]);
-            sb.append(" ");
-        }
+        for(int i=0; i < N;i++)
+            sb.append(tmp[i]).append(" ");
         System.out.println(sb);
     }
 }
