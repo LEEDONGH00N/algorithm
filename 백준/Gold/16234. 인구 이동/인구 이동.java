0371bb2244a8a26befdf015f2d39b9098782fd3b
LEEDONGH00N <123933574+LEEDONGH00N@users.noreply.github.com>
2024-04-09 18:57:02 +0900

    [Gold IV] Title: 인구 이동, Time: 476 ms, Memory: 118844 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -4,25 +4,24 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 class Country{
-    int y; int x; int proportion;
-    public Country(int y, int x, int proportion) {
+    int y; int x;
+    public Country(int y, int x) {
         this.y = y;
         this.x = x;
-        this.proportion = proportion;
     }
 }
 public class Main {
-    static int N, L, R;
+    static boolean flag;
+    static int N, L, R, sum;
     static int[] dy = {-1, 1, 0, 0}, dx = {0, 0, 1, -1};
     static boolean[][] visited;
-    static int[][] map, copy;
-    static List<Country> list;
+    static int[][] map;
+    static List<Country> list = new ArrayList<>();
     static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken()); L = Integer.parseInt(st.nextToken()); R = Integer.parseInt(st.nextToken());
         map = new int[N][N];
-        copy = new int[N][N];
         for(int i = 0; i < N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < N; j++){
@@ -31,69 +30,47 @@ public class Main {
         }
 
     }
-    static void BFS(int y, int x){
-        Queue<Country> queue = new LinkedList<>();
-        queue.add(new Country(y, x, map[y][x]));
-        list.add(new Country(y, x, map[y][x]));
-        visited[y][x] = true;
-        while (!queue.isEmpty()){
-            Country country = queue.poll();
-            for(int d = 0; d < 4; d++){
-                int ny = country.y + dy[d];
-                int nx = country.x + dx[d];
-                if(ny < 0 || nx < 0 || ny >= N || nx >= N || visited[ny][nx]) continue;
-                int diff = Math.abs(map[ny][nx] - country.proportion);
-                if(diff >= L && diff <= R){
-                    Country tmp = new Country(ny, nx, map[ny][nx]);
-                    list.add(tmp);
-                    queue.add(tmp);
-                    visited[ny][nx] = true;
-                }
+    static void dfs(int y, int x){
+        for(int i = 0; i < 4; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny < 0 || nx < 0 || ny >= N || nx >= N || visited[ny][nx]) continue;
+            int diff = Math.abs(map[ny][nx] - map[y][x]);
+            if(diff >= L && diff <= R){
+                sum += map[ny][nx];
+                list.add(new Country(ny, nx));
+                visited[ny][nx] = true;
+                dfs(ny, nx);
             }
         }
+
     }
-    static void proportionRebalance(){
-        double totalProportion = 0; int calculatedProportion;
+    static void populationRebalanced(){
         for(Country country : list){
-            totalProportion += country.proportion;
-        }
-        calculatedProportion = (int) Math.floor(totalProportion / list.size());
-        for(Country country : list){
-            map[country.y][country.x] = calculatedProportion;
-        }
-    }
-    static boolean isMapChanged(){
-        for(int i = 0 ; i < N; i++){
-            if(Arrays.compare(copy[i], map[i]) != 0)
-                return true;
-        }
-        return false;
-    }
-    static void mapCopy(){
-        for(int i = 0 ; i < N; i++){
-            copy[i] = Arrays.copyOf(map[i], N);
+            map[country.y][country.x] = sum/list.size();
+            flag = true;
         }
     }
     public static void main(String[] args) throws IOException {
-        int answer = 0;
         input();
-        mapCopy();
+        int answer = 0;
         while (true){
+            flag = false;
             visited = new boolean[N][N];
-            list = new ArrayList<>();
             for(int i = 0; i < N; i++){
                 for (int j = 0; j < N; j++){
                     if(!visited[i][j]) {
-                        BFS(i, j);
-                        if(list.size() > 1)
-                            proportionRebalance();
                         list.clear();
+                        visited[i][j] = true;
+                        list.add(new Country(i, j));
+                        sum = map[i][j];
+                        dfs(i, j);
+                        if(list.size() == 1) continue;
+                        populationRebalanced();
                     }
                 }
             }
-            if (!isMapChanged())
-                break;
-            mapCopy();
+            if(!flag) break;
             answer++;
         }
         System.out.println(answer);
