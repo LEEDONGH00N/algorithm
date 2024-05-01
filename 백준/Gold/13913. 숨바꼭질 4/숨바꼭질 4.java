a1cdeb9623b8efa1c00157eae9477b01537e1a08
LEEDONGH00N <123933574+LEEDONGH00N@users.noreply.github.com>
2024-05-01 19:05:05 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 1252 ms, Memory: 57432 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -1,16 +1,5 @@
 import java.io.*;
 import java.util.*;
-
-
-class Node{
-    int current;
-    int pre;
-
-    public Node(int current, int pre) {
-        this.current = current;
-        this.pre = pre;
-    }
-}
 public class Main {
     static int N, K;
     static int[] visited, pre;
@@ -19,14 +8,14 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        visited = new int[100001];
-        pre = new int[1000001];
+        visited = new int[2001000];
+        pre = new int[2001000];
     }
     static void solution(){
         Queue<Integer> queue = new ArrayDeque<>();
         queue.offer(N);
         visited[N] = 1;
-        pre[N] = 1000001;
+        pre[N] = 2001000;
         int[] moves = new int[3];
         while (!queue.isEmpty()){
             int current =  queue.poll();
@@ -35,8 +24,11 @@ public class Main {
             moves[1] = current - 1;
             moves[2] = current * 2;
 
+            if(current == K)
+                break;
+
             for(int next : moves){
-                if(0 <= next && next <= 100000) {
+                if(0 <= next && next <= 200100) {
                     if (visited[next] == 0) {
                         queue.offer(next);
                         visited[next] = visited[current] + 1;
@@ -56,7 +48,7 @@ public class Main {
         do {
             list.add(pre[tmp]);
             tmp = pre[tmp];
-        } while (tmp != 1000001);
+        } while (tmp != 2001000);
         for(int i = list.size() - 2; i >= 0; i--){
             System.out.print(list.get(i) + " ");
         }
