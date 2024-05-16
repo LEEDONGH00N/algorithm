07509af7b7ae5677f2faa7d6653b705af6dcd0fa
LEEDONGH00N <123933574+LEEDONGH00N@users.noreply.github.com>
2024-05-16 21:51:58 +0900

    [Gold IV] Title: 주난의 난(難), Time: 228 ms, Memory: 21672 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
@@ -1,19 +1,8 @@
 import java.io.*;
 import java.util.*;
-class Dot{
-    int y, x;
-
-    public Dot(int y, int x) {
-        this.y = y;
-        this.x = x;
-    }
-}
 public class Main {
-    static int N, M;
-    static int jn_y, jn_x;
-    static int choco_y, choco_x;
-    static int[] dy = {-1, 1, 0, 0};
-    static int[] dx = {0, 0, -1, 1};
+    static int N, M, count = 0, jn_y, jn_x, choco_y, choco_x;
+    static int[] dy = {-1, 1, 0, 0}, dx = {0, 0, -1, 1};
     static char[][] map;
     static boolean[][] visited;
 
@@ -28,6 +17,7 @@ public class Main {
         choco_y = Integer.parseInt(st.nextToken());
         choco_x = Integer.parseInt(st.nextToken());
         map = new char[N+2][M+2];
+        visited = new boolean[N+2][M+2];
         for(int i = 1; i <= N; i++){
             String line = br.readLine();
             for(int j = 0; j < line.length(); j++){
@@ -35,54 +25,36 @@ public class Main {
             }
         }
     }
-    static boolean solution() {
-        Set<Dot> dots = new HashSet<>();
-        Queue<Dot> queue = new ArrayDeque<>();
-        queue.offer(new Dot(jn_y, jn_x));
+    static void solution() {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(1000 * jn_y + jn_x);
         visited[jn_y][jn_x] = true;
-        while (!queue.isEmpty()) {
-            Dot dot = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int nx = dot.x + dx[i];
-                int ny = dot.y + dy[i];
-                if (nx <= 0 || nx > M || ny <= 0 || ny > N || visited[ny][nx]) continue;
-                if (map[ny][nx] == '1') {
-                    dots.add(new Dot(ny, nx));
+        while(map[choco_y][choco_x] != '0'){
+            count++;
+            Queue<Integer> temp = new ArrayDeque<>();
+            while (!queue.isEmpty()) {
+                int current = queue.poll();
+                int x = current % 1000;
+                int y = current / 1000;
+                for (int i = 0; i < 4; i++) {
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+                    if (nx <= 0 || nx > M || ny <= 0 || ny > N || visited[ny][nx]) continue;
                     visited[ny][nx] = true;
-                    continue;
+                    if(map[ny][nx] != '0'){
+                        map[ny][nx] = '0';
+                        temp.offer(ny * 1000 + nx);
+                    }
+                    else
+                        queue.offer(ny * 1000 + nx);
                 }
-                else if (map[ny][nx] == '#') {
-                    map[ny][nx] = 'X';
-                    return true;
-                }
-                queue.offer(new Dot(ny, nx));
-                visited[ny][nx] = true;
             }
+            queue = temp;
         }
-        for (Dot dot : dots) {
-            map[dot.y][dot.x] = '0';
-        }
-        return false;
     }
-//    static void printMap(){
-//        for(int i = 1; i <= N; i++){
-//            for(int j = 1; j <= M; j++){
-//                System.out.print(map[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-//    }
     public static void main(String[] args) throws IOException {
         input();
-        int count = 0;
-        while(true){
-            count++;
-            visited = new boolean[N+2][M+2];
-            if(solution()) {
-                System.out.println(count);
-                break;
-            }
-        }
-
+        solution();
+        System.out.println(count);
     }
 }
