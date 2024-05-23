26d21aba6587f4ba1de134eeadd8e779ce500647
LEEDONGH00N <123933574+LEEDONGH00N@users.noreply.github.com>
2024-05-23 19:50:38 +0900

    Update 대칭 차집합.java

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -1,6 +1,17 @@
 import java.io.*;
 import java.util.*;
-
+/**
+ * 접근 방식
+ *
+ * 처음 든 생각은 수학적으로 접근
+ * 집합 A를 담아 둘 임시 집합 tmp를 만들고,
+ * A - B, B - tmp 해서 단순히 사이즈를 더함
+ * 
+ * 그런데 이것보다 입력 받을 때 map을 하나 만들어 원소를 키로, 개수를 값으로 함
+ * 더 나아가, 만약 같은 숫자를 입력하면 그 키를 삭제해버리고
+ * 마지막에 map의 크기만을 반환하면 되겠다 생각
+ * 
+ */
 public class Main {
     static Map<Integer, Integer> result = new HashMap<>();
     static int a, b;
