03d810320df67d364ea713c2e7639407b3e53cb8
LEEDONGH00N <123933574+LEEDONGH00N@users.noreply.github.com>
2025-02-19 19:54:06 +0900

    [Silver III] Title: 회전하는 큐, Time: 108 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -2,48 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static LinkedList<Integer> deque = new LinkedList<>();
-
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        int cnt = 0;
-        for(int i = 1; i <= N; i++) {
-            deque.offer(i);
-        }
-        int[] nums = new int[M];
-        for(int i = 0; i < M; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+
+        Deque<Integer> deque = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            deque.addLast(i);
         }
 
-        for(int i = 0; i < M; i++){
-            int numIdx = deque.indexOf(nums[i]);
-            int middle;
-            if(deque.size() % 2 == 0) {
-                middle = deque.size() / 2;
+        st = new StringTokenizer(br.readLine());
+        int moveCount = 0;
+
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+
+            // 현재 큐에서 목표 숫자의 인덱스 찾기
+            int targetIdx = 0;
+            for (int num : deque) {
+                if (num == target) break;
+                targetIdx++;
             }
-            else {
-                middle = deque.size() / 2 + 1;
-            }
-            if(numIdx < middle){ //2번 계산
-                for(int j = 0; j < numIdx; j++){
-                    int tmp = deque.pollFirst();
-                    deque.offerLast(tmp);
-                    cnt++;
+
+            int leftMove = targetIdx;                 // 왼쪽 이동 횟수 (앞에서 target까지)
+            int rightMove = deque.size() - targetIdx; // 오른쪽 이동 횟수 (뒤에서 target까지)
+
+            if (leftMove <= rightMove) {
+                // 왼쪽 이동이 더 빠른 경우
+                moveCount += leftMove;
+                while (leftMove-- > 0) {
+                    deque.addLast(deque.pollFirst()); // 왼쪽으로 이동
+                }
+            } else {
+                // 오른쪽 이동이 더 빠른 경우
+                moveCount += rightMove;
+                while (rightMove-- > 0) {
+                    deque.addFirst(deque.pollLast()); // 오른쪽으로 이동
                 }
             }
-            else { // 3번 계산
-                for(int j = deque.size() - 1; j >= numIdx; j--){
-                    int tmp = deque.pollLast();
-                    deque.offerFirst(tmp);
-                    cnt++;
-                }
-            }
+
+            // 맨 앞의 요소 제거
             deque.pollFirst();
         }
-        System.out.println(cnt);
+
+        System.out.println(moveCount);
     }
 }
