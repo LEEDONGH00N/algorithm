491a5df05c8f5214d28b9d97561ee8eaf2f67f4a
LEEDONGH00N <123933574+LEEDONGH00N@users.noreply.github.com>
2025-04-08 15:21:00 +0900

    [Silver II] Title: 섬의 개수, Time: 152 ms, Memory: 15680 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -6,56 +6,50 @@ import java.util.*;
 public class Main {
 
     static int count;
-    static int x, y;
-    static int[] dy = {-1, 1, 0 ,0, 1, 1, -1, -1};
-    static int[] dx = {0, 0, 1, -1 ,1, -1, 1, -1};
+    static int W, H;
+    static int[] dh = {-1, 1, 0 ,0, 1, 1, -1, -1};
+    static int[] dw = {0, 0, 1, -1 ,1, -1, 1, -1};
     static int[][] arr;
 
-    static void solution(int x, int y){
-        if (arr[x][y] == 1) {
-            arr[x][y] = 0;
-            for(int i = 0; i < 8; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if(nx >= 0 && ny >= 0 && nx < arr.length && ny < arr[0].length && arr[nx][ny] == 1)
-                    solution(nx, ny);
-            }
+    static void solution(int h, int w){
+        arr[h][w] = 0;
+        for(int i = 0; i < 8; i++){
+            int nh = h + dh[i];
+            int nw = w + dw[i];
+            if(nh >= 0 && nw >= 0 && nh < H && nw < W && arr[nh][nw] == 1)
+                solution(nh, nw);
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
         while(true){
             count = 0;
             st = new StringTokenizer(br.readLine());
-            y = Integer.parseInt(st.nextToken());
-            x = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken());
+            H = Integer.parseInt(st.nextToken());
 
-            if(x == 0 && y == 0)
+            if(W == 0 && H == 0)
                 break;
 
-            arr = new int[x][y];
-            for(int i = 0; i < x; i++){
+            arr = new int[H][W];
+            for(int i = 0; i < H; i++){
                 st = new StringTokenizer(br.readLine());
-                for(int j = 0; j < y; j++)
+                for(int j = 0; j < W; j++)
                     arr[i][j] = Integer.parseInt(st.nextToken());
             }
 
-            for(int i = 0; i < x; i++){
-                for(int j = 0; j < y; j++){
+            for(int i = 0; i < H; i++){
+                for(int j = 0; j < W; j++){
                     if(arr[i][j] == 1) {
                         solution(i, j);
                         count++;
                     }
                 }
             }
-            bw.write(count + "\n");
+            System.out.println(count);
         }
-        bw.flush();
-        bw.close();
-        br.close();
     }
 }
