167377faa8ba8d4963f1d2aa6d918fe0615815c5
LEEDONGHOON <123933574+LEEDONGH00N@users.noreply.github.com>
2025-07-02 16:50:32 +0900

    [Gold V] Title: 행복 유치원, Time: 516 ms, Memory: 54216 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.java
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -7,19 +7,18 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         int[] heights = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             heights[i] = Integer.parseInt(st.nextToken());
         }
-        int[] diffs = new int[N - 1];
         for (int i = 0; i < N - 1; i++) {
-            diffs[i] = heights[i + 1] - heights[i];
+            pq.offer(heights[i + 1] - heights[i]);
         }
-        Arrays.sort(diffs);
         int sum = 0;
         for (int i = 0; i < N - K; i++) {
-            sum += diffs[i];
+            sum += pq.poll();
         }
         System.out.println(sum);
     }
