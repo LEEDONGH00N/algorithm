00aee3238ba4fd8f8dd9bff0ab0091d7d7f131c9
LEEDONGHOON <123933574+LEEDONGH00N@users.noreply.github.com>
2025-07-28 16:23:58 +0900

    [Gold III] Title: 레이저 통신, Time: 112 ms, Memory: 15120 KB -BaekjoonHub

--- 백준/Gold/6087. 레이저 통신/레이저 통신.java
+++ 백준/Gold/6087. 레이저 통신/레이저 통신.java
@@ -1,18 +1,13 @@
 import java.io.*;
 import java.util.*;
 
-class Point implements Comparable<Point>{
+class Point{
     int y, x, dir, cost;
     Point(int y, int x, int dir, int cost) {
-        this.y = y;
-        this.x = x;
+        this.y   = y;
+        this.x   = x;
         this.dir = dir;
         this.cost = cost;
-    }
-
-    @Override
-    public int compareTo(Point o) {
-        return this.cost - o.cost;
     }
 }
 
@@ -21,69 +16,63 @@ public class Main {
     static int W, H;
     static final int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
     static char[][] map;
-    static int[][] cost0, cost1, cost2, cost3;
+    static int[][][] cost;
     static Queue<int[]> laser = new ArrayDeque<>();
 
     public static void main(String[] args) throws IOException {
         input();
-        int[] start = laser.poll(); int[] end = laser.poll();
+        int[] start = laser.poll();
+        int[] end   = laser.poll();
         bfs(start, end);
     }
 
     static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        W = Integer.parseInt(st.nextToken());H = Integer.parseInt(st.nextToken());
-        map = new char[H][W];
-        cost0 = new int[H][W];cost1 = new int[H][W];cost2 = new int[H][W];cost3 = new int[H][W];
+        W = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+        map  = new char[H][W];
+        cost = new int[4][H][W];
+        for (int d = 0; d < 4; d++)
+            for (int i = 0; i < H; i++)
+                Arrays.fill(cost[d][i], Integer.MAX_VALUE);
+
         for (int i = 0; i < H; i++) {
             String line = br.readLine();
             for (int j = 0; j < W; j++) {
                 map[i][j] = line.charAt(j);
                 if (map[i][j] == 'C') laser.offer(new int[]{i, j});
-                cost0[i][j] = cost1[i][j] = cost2[i][j] = cost3[i][j] = Integer.MAX_VALUE;
             }
         }
     }
 
-    static void bfs(int[] s, int[] e) {
-        PriorityQueue<Point> queue = new PriorityQueue<>();
-        cost0[s[0]][s[1]] = cost1[s[0]][s[1]] = cost2[s[0]][s[1]] = cost3[s[0]][s[1]] = 0;
-        queue.offer(new Point(s[0], s[1], -1, 0));
-        while (!queue.isEmpty()) {
-            Point p = queue.poll();
-            if (p.y == e[0] && p.x == e[1]) {
-                int ans = Math.min(
-                        Math.min(cost0[p.y][p.x], cost1[p.y][p.x]),
-                        Math.min(cost2[p.y][p.x], cost3[p.y][p.x])
-                );
-                System.out.println(ans);
+    static void bfs(int[] start, int[] end) {
+        Deque<Point> dq = new ArrayDeque<>();
+        for (int d = 0; d < 4; d++)
+            cost[d][start[0]][start[1]] = 0;
+        dq.offerFirst(new Point(start[0], start[1], -1, 0));
+
+        while (!dq.isEmpty()) {
+            Point p = dq.pollFirst();
+            if (p.y == end[0] && p.x == end[1]) {
+                int a = cost[0][p.y][p.x], b = cost[1][p.y][p.x];
+                int c = cost[2][p.y][p.x], d = cost[3][p.y][p.x];
+                System.out.println(Math.min(Math.min(a,b), Math.min(c,d)));
                 return;
             }
+
             for (int d = 0; d < 4; d++) {
-                int ny = p.y + dirs[d][0];
-                int nx = p.x + dirs[d][1];
+                int ny = p.y + dirs[d][0], nx = p.x + dirs[d][1];
                 if (ny < 0 || ny >= H || nx < 0 || nx >= W) continue;
                 if (map[ny][nx] == '*') continue;
-                int nextCost;
-                if (p.dir == -1 || p.dir == d) {
-                    nextCost = p.cost;
-                } else {
-                    nextCost = p.cost + 1;
-                }
-                int[][] target;
-                if (d == 0) {
-                    target = cost0;
-                } else if (d == 1) {
-                    target = cost1;
-                } else if (d == 2) {
-                    target = cost2;
-                } else {
-                    target = cost3;
-                }
-                if (nextCost < target[ny][nx]) {
-                    target[ny][nx] = nextCost;
-                    queue.offer(new Point(ny, nx, d, nextCost));
+
+                int nextCost = (p.dir == -1 || p.dir == d) ? p.cost : p.cost + 1;
+                if (nextCost < cost[d][ny][nx]) {
+                    cost[d][ny][nx] = nextCost;
+                    Point nxt = new Point(ny, nx, d, nextCost);
+
+                    if (nextCost == p.cost) dq.offerFirst(nxt); // 가중치 0
+                    else                    dq.offerLast(nxt);  // 가중치 1
                 }
             }
         }
