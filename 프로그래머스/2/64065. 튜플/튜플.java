be11f5e8f356c625f3303370ba33fca32590f9ca
LEEDONGHOON <123933574+LEEDONGH00N@users.noreply.github.com>
2026-03-21 00:50:23 +0900

    [level 2] Title: 튜플, Time: 90.21 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -8,10 +8,6 @@ class Solution {
         Stack<Character> stack = new Stack<>();
         List<List<Integer>> tuples = new ArrayList<>();   
         
-        
-        for(int i = 0; i <= 500; i++)
-            tuples.add(new ArrayList<>());
-        
         int num = 0, t = 0;
         
         for(int i = 0; i < s.length(); i++){
@@ -19,32 +15,30 @@ class Solution {
             Character cur = s.charAt(i);
             if (cur == '{') {
                 stack.push('{');
-                continue;
+                if (tuples.size() <= t) {
+                    tuples.add(new ArrayList<>());
+                }
             }
             else if (cur == '}') {
                 if(num != 0) tuples.get(t).add(num);
                 num = 0;
                 t++;
                 stack.pop();
-                continue;
             }
             else if (Character.isDigit(cur)){
                 num = num * 10 
                     + Integer.parseInt(Character.toString(cur));
-                continue;
             }
             else if(cur == ','){
                 if(stack.isEmpty()) continue;
                 tuples.get(t).add(num);
                 num = 0;
-                continue;
             }
             
         }
         tuples.sort((a, b) -> a.size() - b.size());
         List<Integer> answer = new ArrayList<>();
         for(List<Integer> list : tuples){
-            if (list.isEmpty()) continue;
             if(answer.isEmpty()){
                 answer.add(list.get(0));
                 continue;
