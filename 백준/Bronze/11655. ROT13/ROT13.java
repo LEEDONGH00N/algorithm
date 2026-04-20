4a0d49faf989d1fe8e007afcf3a5c4ad20b0e05c
LEEDONGHOON <123933574+LEEDONGH00N@users.noreply.github.com>
2026-04-20 21:46:40 +0900

    [Bronze I] Title: ROT13, Time: 104 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < line.length(); i++){
+            char c = line.charAt(i);
+            if ('A' <= c && c <= 'Z'){
+                c = (char) ('A' + ((c - 'A' + 13) % 26));
+            }
+            else if ('a' <= c && c <= 'z'){
+                c = (char) ('a' + ((c - 'a' + 13) % 26));
+            }
+            sb.append(c);
+        }
+        System.out.println(sb);
+    }
+}
